ac7ad518bec9ad025e1466b1b759d8d40a5c1342
Nicolas Alayo <n_alayo_arias@outlook.com>
2025-06-21 18:02:04 -0500

    Add Eureka client support and update application configuration for ms-mascotas module

--- veterinaria-app/ms-mascotas/src/main/java/com/veterinaria/ms_mascotas/MsMascotasApplication.java
+++ veterinaria-app/ms-mascotas/src/main/java/com/veterinaria/ms_mascotas/MsMascotasApplication.java
@@ -2,7 +2,9 @@ package com.veterinaria.ms_mascotas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MsMascotasApplication {
 
