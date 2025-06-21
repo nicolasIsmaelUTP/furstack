c47a1749d9b858590bc64627318ae2940e670267
Nicolas Alayo <n_alayo_arias@outlook.com>
2025-06-21 17:50:53 -0500

    Add ms-mascotas module with initial configuration and structure

---
+++ veterinaria-app/ms-mascotas/src/main/java/com/veterinaria/ms_mascotas/MsMascotasApplication.java
@@ -0,0 +1,13 @@
+package com.veterinaria.ms_mascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMascotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMascotasApplication.class, args);
+	}
+
+}
---
+++ veterinaria-app/ms-mascotas/src/test/java/com/veterinaria/ms_mascotas/MsMascotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veterinaria.ms_mascotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMascotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
