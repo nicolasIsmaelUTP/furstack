16d235246c99b2b48c8d4d28b164a048b2d93020
Nicolas Alayo <n_alayo_arias@outlook.com>
2025-06-21 18:09:03 -0500

    Add ms-duenios application configuration and enable Eureka discovery client

--- veterinaria-app/ms-duenios/src/main/java/com/veterinaria/ms_duenios/MsDueniosApplication.java
+++ veterinaria-app/ms-duenios/src/main/java/com/veterinaria/ms_duenios/MsDueniosApplication.java
@@ -2,7 +2,9 @@ package com.veterinaria.ms_duenios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MsDueniosApplication {
 
