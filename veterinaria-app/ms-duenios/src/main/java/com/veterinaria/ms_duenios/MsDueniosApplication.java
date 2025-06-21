befa559aa4f4b9ff0f93af026776a0290b27230e
Nicolas Alayo <n_alayo_arias@outlook.com>
2025-06-21 18:06:34 -0500

    Add ms-duenios module with initial configuration and structure

---
+++ veterinaria-app/ms-duenios/src/main/java/com/veterinaria/ms_duenios/MsDueniosApplication.java
@@ -0,0 +1,13 @@
+package com.veterinaria.ms_duenios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDueniosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDueniosApplication.class, args);
+	}
+
+}
---
+++ veterinaria-app/ms-duenios/src/test/java/com/veterinaria/ms_duenios/MsDueniosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veterinaria.ms_duenios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDueniosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
